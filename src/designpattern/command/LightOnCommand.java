e7b1c34c7094b60f56d88b7a84d0cd387ac80f61
Picbel <picbel.gon@gmail.com>
2022-01-12 22:52:53 +0900

    command 패턴 예제 적용

---
+++ src/designpattern/command/LightOnCommand.java
@@ -0,0 +1,14 @@
+package designpattern.command;
+
+public class LightOnCommand implements Command{
+
+    Light light;
+
+    public LightOnCommand(Light light) {
+        this.light = light;
+    }
+    @Override
+    public void execute() {
+        light.on();
+    }
+}
---
+++ src/designpattern/command/testCode.java
@@ -0,0 +1,12 @@
+package designpattern.command;
+
+public class testCode {
+    public static void main(String[] args) {
+        SimpleRemoteControl remote = new SimpleRemoteControl();
+        Light light = new Light();
+        LightOnCommand lightOn = new LightOnCommand(light);
+
+        remote.setCommand(lightOn);
+        remote.buttonWasPressed();
+    }
+}
